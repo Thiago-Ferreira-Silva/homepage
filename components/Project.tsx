49078fff5bf2e810e31d40a8a59503c24128b789
Thiago-Ferreira-Silva <62727996+Thiago-Ferreira-Silva@users.noreply.github.com>
2021-03-03 11:00:18 -0300

    Implements the Project component

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,17 +1,24 @@
+import Image from 'next/image'
+
 import styles from '../styles/components/Project.module.scss'
 
 interface ProjectProps {
     name: string
     description: string
-    link: string
+    project: string
     github: string
     image: string
 } // check if there are better types
 
-export default function Project({ name, description, link, github, image }: ProjectProps) {
+export default function Project({ name, description, project, github, image }: ProjectProps) {
     return (
-        <div className="contaier">
-            Project
+        <div className={styles.container}>
+            <Image src={image} width='200' height='200' alt="Project"/>
+            <strong>{name}</strong>
+            <p>{description}</p>
+            <a href={project} target="_blank">Link to this project</a>
+            <br/>
+            <a href={github} target="_blank">Project on GitHub</a>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
 import Head from 'next/head'
+import fs from 'fs'
+import path from 'path'
 
 import styles from '../styles/pages/Home.module.scss'
+import Project from '../components/Project'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+interface Project {
+  name: string
+  description: string
+  project: string
+  github: string
+  image: string
+} //talvez usar context API para não ter que repetir isso
+
+export default function Home({ projects }) {
+
+const [ projectsJSX, setProjectsJSX ] = useState(null)
+
+useEffect(() => {
+  const projectsJSX = projects.map((project: Project) => <Project {...project} key={Math.random()} />)
+  setProjectsJSX(projectsJSX)
+}, [])
+
   return (
     <div>
       <Head>
@@ -25,8 +45,7 @@ export default function Home() {
         <section>
           <h2>My projects</h2>
           <ul>
-            <li>Create a project component</li>
-            <li>Pass image, link, GitHub link, name and description via props</li>
+            { projectsJSX }
           </ul>
         </section>
 
@@ -36,4 +55,14 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+// add projects and blog posts dynamically
+export async function getStaticProps() {
+  const fullPath = path.join(process.cwd(), 'data', 'projects.json')
+  const projects = JSON.parse(fs.readFileSync(fullPath, 'utf-8'))
+
+  return {
+      props: { projects }
+  }
 }
