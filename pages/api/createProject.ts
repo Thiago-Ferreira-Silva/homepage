92aad3a7a5ddb62dae9c2c6323a500596c86a234
Thiago-Ferreira-Silva <62727996+Thiago-Ferreira-Silva@users.noreply.github.com>
2021-03-27 12:45:27 -0300

    Fix images paths 2

--- pages/api/createProject.ts
+++ pages/api/createProject.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs'
+import path from 'path'
 import { ProjectsController } from '../../server/controllers/projectsController'
 
 const projectsController = new ProjectsController()
@@ -13,7 +14,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     delete project.imageFile
 
     const response = await projectsController.createProject(project, () => {
-        fs.writeFileSync( __dirname + `../../public/images/${project.name.toLowerCase()}.png`, base64Image, { encoding: 'base64' })
+        fs.writeFileSync( path.join(__dirname + `../../public/images/${project.name.toLowerCase()}.png`), base64Image, { encoding: 'base64' })
     })
 
     return res.send(response)
--- pages/api/deleteProject.ts
+++ pages/api/deleteProject.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs'
 import { ProjectsController } from '../../server/controllers/projectsController'
+import path from 'path'
 
 const projectsController = new ProjectsController()
 
@@ -8,7 +9,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     const name = req.body.name
 
     const response = await projectsController.deleteProject(name, () => {
-        fs.unlinkSync( __dirname + `../../public/images/${name}.png`)
+        fs.unlinkSync( path.join(__dirname + `../../public/images/${name}.png`) )
     })
     return res.send(response)
 }
